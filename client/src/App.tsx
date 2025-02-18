77c8d544a0e9434a86c73f9978d71b31a29c59a1
Wilfred K <83074782+wilfredkiumi@users.noreply.github.com>
2025-02-18 21:39:16 +0000

    Restored to 'fb0ab3c43782229cd3aa5565e729f7ce4c7fd388'
    Replit-Restored-To:fb0ab3c43782229cd3aa5565e729f7ce4c7fd388

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -24,7 +24,6 @@ import NotificationsPage from "@/pages/notifications-page"; // Import the new co
 import i18n from './lib/i18n'; // Import i18n instance
 import { I18nextProvider } from 'react-i18next';
 import CreateEventPage from "@/pages/create-event"; // Import the new component
-import PWAPrompt from "@/components/pwa-prompt"; //Import the PWA prompt component
 
 
 function Router() {
@@ -62,7 +61,6 @@ function App() {
           <AuthProvider>
             <ChatProvider>
               <Router />
-              <PWAPrompt />
               <Toaster />
             </ChatProvider>
           </AuthProvider>
